5ac6b4f68754a532004fac0daea05aafb2f1abc8
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2018-05-16 12:47:46 -0300

    Create Pago.java

---
+++ modelo/Pago.java
@@ -0,0 +1,18 @@
+package modelo;
+
+public class Pago {
+	
+	private int idLista;
+	private int idParticipante;
+	private float monto;
+	
+	public Pago(int idLista, int idParticipante, float monto) {
+		super();
+		this.idLista = idLista;
+		this.idParticipante = idParticipante;
+		this.monto = monto;
+	}
+	
+	
+
+}
