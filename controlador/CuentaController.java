7be03aee322b4cfafd4ae4b2b788b92228684ef0
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2018-05-16 17:18:29 -0300

    Create CuentaController.java

---
+++ controlador/CuentaController.java
@@ -0,0 +1,25 @@
+package controlador;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import modelo.Cuenta;
+
+public class CuentaController {
+	
+	private static ListaController instancia;
+	private ArrayList<Cuenta> cuentas;
+	
+	static public ListaController getInstancia() {
+		if(CuentaController.instancia == null) {
+			CuentaController.instancia = new ListaController();
+		}
+		return CuentaController.instancia;
+	}
+	
+	public void altaCuenta(int idCuenta, int idLista, Date fechaPago, float monto){
+		Cuenta c = new Cuenta (idCuenta, idLista, fechaPago, monto);
+		cuentas.add(c);
+	}
+
+}
