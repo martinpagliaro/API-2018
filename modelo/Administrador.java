6710d6c95d445e3c877cc92e413b0fbdfdd8f34b
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2018-05-16 12:42:32 -0300

    Update Administrador.java

--- modelo/Administrador.java
+++ modelo/Administrador.java
@@ -1,1 +1,13 @@
+package modelo;
+
+import java.sql.Date;
+
+public class Administrador extends Usuario{
+
+	public Administrador(int idUsuario, String nombre, Date fechaNac, String mail, Password password, int idRole,
+			boolean estadoUsuario) {
+		super(idUsuario, nombre, fechaNac, mail, password, idRole, estadoUsuario);
+	}
+
+}
 
