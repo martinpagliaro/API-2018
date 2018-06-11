99e26834ab72c9ba7f2a27f96f3808eaef8739ce
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2018-06-11 16:52:32 -0300

    Update ListaController.java

--- controlador/ListaController.java
+++ controlador/ListaController.java
@@ -1,8 +1,11 @@
 package controlador;
 
 import java.util.ArrayList;
+import java.util.Date;
 
+import modelo.Usuario;
 import modelo.Lista;
+import modelo.Participante;
 
 public class ListaController {
 	
@@ -16,11 +19,59 @@ public class ListaController {
 		return ListaController.instancia;
 	}
 	
-	public Lista buscarLista(int idLista){
+	public Lista buscarLista(String nombreLista){
 		for (int i=0;i<listas.size();i++){
 			Lista l = listas.get(i);
-			if (l.esLista(idLista))
+			if (l.esLista(nombreLista))
 				return l;
+		}
+		return null;
+	}
+		
+	/*Alta de lista*/
+	public int altaLista (Usuario administrador, String nombreLista, String nombreAgasajado,
+			Date fechaNacAgasajado, String mailAgasajado, ArrayList<Participante> participantes, float montoTotal,
+			float montoPorParticipante, float montoRecaudado, Date fechaInicio, Date fechaFin, String estadoLista){
+		if (buscarLista(nombreLista) == null) {
+			Lista l = new Lista (administrador, nombreLista, nombreAgasajado,
+					fechaNacAgasajado, mailAgasajado, participantes, montoTotal,
+					montoPorParticipante, montoRecaudado, fechaInicio, fechaFin, estadoLista);
+			listas.add(l);
+			return 0; //Lista creada
+		}
+		return 1; //Ya existe la lista
+	}
+	
+	/*Baja de lista*/
+	public boolean bajaLista (String nombreLista){
+		Lista l = buscarLista(nombreLista);
+		if (l!=null) {
+			listas.remove(l);
+			return true;
+		}
+		return false;
+	}
+	
+	/*Modificacion de lista*/
+	public int modificarLista(String nombreLista, String nombreAgasajado,
+			String mailAgasajado, Date fechaNacAgasajado, Date fechaFin, float montoTotal) {
+		Lista l = buscarLista(nombreLista);
+		if (l!=null){
+			l.setFechaFin(fechaFin);
+			l.setMailAgasajado(mailAgasajado);
+			l.setNombreAgasajado(nombreAgasajado);
+			l.setNombreLista(nombreLista);
+			l.setFechaNacAgasajado(fechaNacAgasajado);
+			l.setMontoTotal(montoTotal);
+			return 0;
+		}
+		return 1;
+	}
+	
+	public ListaView mostrarLista (String nombreLista){
+		Lista lista = buscarLista(nombreLista);
+		if (lista!=null) {
+			return lista.crearVista();
 		}
 		return null;
 	}
