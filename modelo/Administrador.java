f93ee31e444cbc0b66b3bffa0b82db7c04c7ceeb
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2018-06-11 17:01:31 -0300

    Update Administrador.java

--- modelo/Administrador.java
+++ modelo/Administrador.java
@@ -1,13 +1,21 @@
 package modelo;
 
-import java.sql.Date;
+import controlador.UsuarioView;
+import persistencia.AdmPersistenciaUsuario;
 
 public class Administrador extends Usuario{
-
-	public Administrador(int idUsuario, String nombre, Date fechaNac, String mail, Password password, int idRole,
-			boolean estadoUsuario) {
-		super(idUsuario, nombre, fechaNac, mail, password, idRole, estadoUsuario);
+	
+	public Administrador(String nombreDeUsuario, String passwordString) {
+		super(nombreDeUsuario, passwordString);
+		//AdmPersistenciaUsuario.getInstancia().insertUsuario(this);
 	}
 
-}
+	public UsuarioView getUsuarioView() {
+		return (new UsuarioView(this.nombreDeUsuario, this.estadoUsuario));
+	}
 
+	public String getNombre() {
+		return nombreDeUsuario;
+	}
+	
+}
