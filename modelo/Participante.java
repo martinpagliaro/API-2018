ec4751553fe07e3704e237a4ffab742c45b0ebe0
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2018-06-11 17:03:37 -0300

    Update Participante.java

--- modelo/Participante.java
+++ modelo/Participante.java
@@ -1,17 +1,39 @@
 package modelo;
 
-import java.sql.Date;
+
+import java.util.Date;
+
+import controlador.UsuarioView;
 
 public class Participante extends Usuario{
-	private int idLista;
+	private String nombre;
+	private String mail;
+	private Date fechaNac;
 	private boolean pagado;
 	private float montoPago;
 		
-	public Participante(int idUsuario, String nombre, Date fechaNac, String mail, Password password, int idRole,
-			boolean estadoUsuario, boolean pagado, float montoPago) {
-		super(idUsuario, nombre, fechaNac, mail, password, idRole, estadoUsuario);
-		this.pagado = pagado;
-		this.montoPago = montoPago;
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public Date getFechaNac() {
+		return fechaNac;
+	}
+
+	public void setFechaNac(Date fechaNac) {
+		this.fechaNac = fechaNac;
 	}
 
 	public boolean isPagado() {
@@ -21,10 +43,36 @@ public class Participante extends Usuario{
 	public void setPagado(boolean pagado) {
 		this.pagado = pagado;
 	}
-	
-	public boolean esParticipante(int idUsuario, int idLista){
-		return this.idUsuario == idUsuario && this.idLista == idLista;
+
+	public float getMontoPago() {
+		return montoPago;
+	}
+
+	public void setMontoPago(float montoPago) {
+		this.montoPago = montoPago;
+	}
+
+	public UsuarioView getUsuarioView() {
+		return (new UsuarioView(this.nombre, this.fechaNac, this.mail, this.nombreDeUsuario,this.estadoUsuario));
 	}
 	
-
+	public Participante(String nombre, String mail, Date fechaNac,
+			String nombreDeUsuario, String passwordString) {
+		super(nombreDeUsuario, passwordString);
+		this.nombre = nombre;
+		this.mail = mail;
+		this.fechaNac = fechaNac;
+		//AdmPersistenciaUsuario.getInstancia().insertUsuario(this);
+	}
+	
+	public Participante(String nombreDeUsuario, String password, Date fechaCreacion, boolean estadoUsuario,
+			String nombre, String mail, Date fechaNac, boolean pagado, float montoPago) {
+		super(nombreDeUsuario, password, fechaCreacion, estadoUsuario);
+		this.nombre = nombre;
+		this.mail = mail;
+		this.fechaNac = fechaNac;
+		this.pagado = pagado;
+		this.montoPago = montoPago;
+	}
+	
 }
