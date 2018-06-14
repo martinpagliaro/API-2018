a8c0a4214af6441b6606f4fd7117a680ba9d6f4e
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2018-06-14 13:38:17 -0300

    Update VistaModificarLista.java

--- vista/VistaModificarLista.java
+++ vista/VistaModificarLista.java
@@ -1,144 +1,238 @@
 package vista;
 
-import java.awt.TextField;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 
 import controlador.ListaController;
 import controlador.ListaView;
 
 public class VistaModificarLista extends JFrame{
-	
-private static final long serialVersionUID = -228100595448307234L;
-	
-	private JPanel contentPane;
-	private String nombreLista;
 
-	/**
-	 * Create the frame.
-	 */
-	public VistaModificarLista() {
-		setTitle("Modificar Lista");
-		setResizable(false);
-		setBounds(100, 100, 456, 251);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		ListaView lv = ListaController.getInstancia().mostrarLista(nombreLista);
-		
-		
-		JLabel lblNombreLista = new JLabel("Nombre lista");
-		lblNombreLista.setBounds(10, 11, 83, 14);
-		contentPane.add(lblNombreLista);
-		
-		JLabel lblNombreAgasajado = new JLabel("Nombre agasajado");
-		lblNombreAgasajado.setBounds(10, 36, 95, 14);
-		contentPane.add(lblNombreAgasajado);
-		
-		JLabel lblFechaNacAgasajado = new JLabel("Fecha de naci del agasajado");
-		lblFechaNacAgasajado.setBounds(10, 61, 146, 14);
-		contentPane.add(lblFechaNacAgasajado);
-		
-		JLabel lblmailAgasajado = new JLabel("Mail agas.");
-		lblmailAgasajado.setBounds(10, 86, 83, 14);
-		contentPane.add(lblmailAgasajado);
-		
-		JLabel lblMontoTotal = new JLabel("Monto total");
-		lblMontoTotal.setBounds(10, 111, 83, 14);
-		contentPane.add(lblMontoTotal);
-		
-		JLabel lblFechaFin = new JLabel("Fecha fin");
-		lblFechaFin.setBounds(10, 136, 83, 14);
-		contentPane.add(lblFechaFin);
-		
-		TextField textNombreLista = new TextField();
-		textNombreLista.setBounds(158, 3, 247, 22);
-		contentPane.add(textNombreLista);
-		
-		TextField textNombreAgasajado = new TextField();
-		textNombreAgasajado.setBounds(158, 28, 247, 22);
-		contentPane.add(textNombreAgasajado);
-		
-		TextField textFechaNacAgasajado = new TextField();
-		textFechaNacAgasajado.setBounds(158, 53, 247, 22);
-		contentPane.add(textFechaNacAgasajado);
-		
-		TextField textMailAgasajado = new TextField();
-		textMailAgasajado.setBounds(158, 78, 247, 22);
-		contentPane.add(textMailAgasajado);
-		
-		TextField textMontoTotal = new TextField();
-		textMontoTotal.setBounds(158, 103, 247, 22);
-		contentPane.add(textMontoTotal);
-		
-		TextField textFechaFin = new TextField();
-		textFechaFin.setBounds(158, 128, 247, 22);
-		contentPane.add(textFechaFin);
-		
-		if (lv != null) {
-			textNombreLista.setText(lv.getNombreLista());
-			textNombreAgasajado.setText(lv.getNombreAgasajado());
-			DateFormat format = new SimpleDateFormat("dd/MM/yyyy", Locale.ROOT);
-			String aux = format.format(lv.getFechaNacAgasajado());
-			textFechaNacAgasajado.setText(aux);
-			textMailAgasajado.setText(lv.getMailAgasajado());
-			textMontoTotal.setText(String.valueOf(lv.getMontoTotal()));
-			String aux2 = format.format(lv.getFechaFin());
-			textFechaFin.setText(aux2);
-		}
-		else {
+	private static final long serialVersionUID = -2921602399012011283L;
+	private JLabel labelNombreLista;
+	private JLabel labelNombreAgasajado;
+	private JLabel labelFechaNacAgasajado;
+	private JLabel labelMailAgasajado;
+	private JLabel labelMontoTotal;
+	private JLabel labelFechaFin;
+	private JTextField textFieldNombreLista;
+	private JTextField textFieldNombreAgasajado;
+	private JTextField textFieldFechaNacAgasajado;
+	private JTextField textFieldMaiAgasajado;
+	private JTextField textFieldMontoTotal;
+	private JTextField textFieldFechaFin;
+	private JButton botonBuscar;
+	private JButton botonModificar;
+	private ListaView lv;
 
-		}
+	public VistaModificarLista()
+	{
+		crearPantalla();
+	}
+
+	private void crearPantalla()
+	{
+		try
+		{
+			setTitle("Modificar Lista");
+			setSize(402, 315);
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			getContentPane().setLayout(null);
+
+			labelNombreLista = new JLabel();
+			getContentPane().add(labelNombreLista);
+			labelNombreLista.setText("Nombre de lista:");
+			labelNombreLista.setBounds(7, 11, 103, 28);
+
+			textFieldNombreLista = new JTextField();
+			textFieldNombreLista.setBounds(120, 15, 143, 20);
+			getContentPane().add(textFieldNombreLista);
+			textFieldNombreLista.setColumns(10);
+			
+			botonBuscar = new JButton("buscar");
+			getContentPane().add(botonBuscar);
+			botonBuscar.setBounds(273, 14, 103, 22);
+			botonBuscar.addActionListener(new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent e)
+				{
+					lv = ListaController.getInstancia().mostrarLista(textFieldNombreLista.getText());
+					if (lv != null)
+					{
+						textFieldNombreLista.setEnabled(false);
+						botonBuscar.setEnabled(false);
+						
+						labelNombreAgasajado.setVisible(true);
+						labelFechaNacAgasajado.setVisible(true);
+						labelMailAgasajado.setVisible(true);
+						labelMontoTotal.setVisible(true);
+						labelFechaFin.setVisible(true);
+						
+						botonModificar.setVisible(true);
+						botonModificar.setEnabled(true);
+						
+						textFieldNombreAgasajado.setVisible(true);
+						textFieldNombreAgasajado.setEnabled(true);
+						textFieldNombreAgasajado.setText(lv.getNombreAgasajado());
+						
+						textFieldFechaNacAgasajado.setVisible(true);
+						textFieldFechaNacAgasajado.setEnabled(true);
+						SimpleDateFormat dateFormatter; 
+						dateFormatter = new SimpleDateFormat("dd/MM/yyyy", Locale.ROOT);
+						textFieldFechaNacAgasajado.setText(dateFormatter.format (lv.getFechaNacAgasajado()));
+						
+						textFieldMaiAgasajado.setVisible(true);
+						textFieldMaiAgasajado.setEnabled(true);
+						textFieldMaiAgasajado.setText(lv.getMailAgasajado());
+						
+						textFieldMontoTotal.setVisible(true);
+						textFieldMontoTotal.setEnabled(true);
+						textFieldMontoTotal.setText(String.valueOf(lv.getMontoTotal()));
+						
+						textFieldFechaFin.setVisible(true);
+						textFieldFechaFin.setEnabled(true);
+						textFieldFechaFin.setText(dateFormatter.format (lv.getFechaFin()));
+						
+					}else
+					{
+						JOptionPane.showMessageDialog((Component)e.getSource(), "Lista inexistente");
+					}
+				}
+
+			});
+
+			labelNombreAgasajado = new JLabel("Nombre agasajado:");
+			labelNombreAgasajado.setBounds(7, 68, 100, 14);
+			getContentPane().add(labelNombreAgasajado);
+			labelNombreAgasajado.setVisible(true);
 		
-		JLabel lblMensaje = new JLabel("");
-		lblMensaje.setBounds(73, 189, 332, 22);
-		contentPane.add(lblMensaje);
-		
-		JButton btnConfirmarCambios = new JButton("Confirmar cambios");
-		btnConfirmarCambios.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if (! textNombreLista.getText().isEmpty() & ! textNombreAgasajado.getText().isEmpty() & ! textFechaNacAgasajado.getText().isEmpty() 
-						& ! textMailAgasajado.getText().isEmpty() & ! textMontoTotal.getText().isEmpty() & ! textFechaFin.getText().isEmpty()) {
-					int resPassRst = -1;
+			labelFechaNacAgasajado = new JLabel("F. nac. agasajado:");
+			labelFechaNacAgasajado.setBounds(10, 100, 114, 14);
+			getContentPane().add(labelFechaNacAgasajado);
+			labelFechaNacAgasajado.setVisible(true);
+
+			labelMailAgasajado = new JLabel("E-Mail agasajado:");
+			labelMailAgasajado.setBounds(10, 135, 103, 14);
+			getContentPane().add(labelMailAgasajado);
+			labelMailAgasajado.setVisible(true);
+			
+			labelMontoTotal = new JLabel("Monto:");
+			labelMontoTotal.setBounds(10, 168, 83, 14);
+			getContentPane().add(labelMontoTotal);
+			labelMontoTotal.setVisible(true);
+			
+			labelFechaFin = new JLabel("Fecha fin:");
+			labelFechaFin.setBounds(10, 197, 83, 14);
+			getContentPane().add(labelFechaFin);
+			labelFechaFin.setVisible(true);
+
+			textFieldNombreAgasajado = new JTextField();
+			textFieldNombreAgasajado.setVisible(true);
+			textFieldNombreAgasajado.setEnabled(false);
+			textFieldNombreAgasajado.setBounds(117, 64, 247, 22);
+			getContentPane().add(textFieldNombreAgasajado);
+			
+			textFieldFechaNacAgasajado = new JTextField();
+			textFieldFechaNacAgasajado.setBounds(117, 96, 247, 22);
+			textFieldFechaNacAgasajado.setVisible(true);
+			textFieldFechaNacAgasajado.setEnabled(false);
+			getContentPane().add(textFieldFechaNacAgasajado);
+			
+			textFieldMaiAgasajado = new JTextField();
+			textFieldMaiAgasajado.setBounds(117, 125, 247, 22);
+			textFieldMaiAgasajado.setVisible(true);
+			textFieldMaiAgasajado.setEnabled(false);
+			getContentPane().add(textFieldMaiAgasajado);
+			
+			textFieldMontoTotal = new JTextField();
+			textFieldMontoTotal.setBounds(117, 156, 247, 22);
+			textFieldMontoTotal.setVisible(true);
+			textFieldMontoTotal.setEnabled(false);
+			getContentPane().add(textFieldMontoTotal);
+			
+			textFieldFechaFin = new JTextField();
+			textFieldFechaFin.setBounds(117, 189, 247, 22);
+			textFieldFechaFin.setVisible(true);
+			textFieldFechaFin.setEnabled(false);
+			getContentPane().add(textFieldFechaFin);
+
+			botonModificar = new JButton("Modificar");
+			botonModificar.setBounds(24, 242, 89, 23);
+			getContentPane().add(botonModificar);
+			botonModificar.setVisible(true);
+			botonModificar.setEnabled(false);
+			botonModificar.addActionListener(new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent e)
+				{
 					DateFormat format = new SimpleDateFormat("dd/MM/yyyy", Locale.ROOT);
-					int resModificar;
+					Date fechaNacAgasajado;
+					Date fechaFin;
 					try {
-						resModificar = ListaController.getInstancia().modificarLista(textNombreLista.getText(), textNombreAgasajado.getText(), textMailAgasajado.getText(), format.parse(textFechaNacAgasajado.getText()), format.parse(textFechaFin.getText()), Float.valueOf(textMontoTotal.getText()));
-					if (resModificar == 0 & resPassRst <= 0) {
-						lblMensaje.setText("Info modificada.");
+						fechaNacAgasajado = format.parse(textFieldFechaNacAgasajado.getText());
+						fechaFin = format.parse(textFieldFechaFin.getText());
+						ListaController.getInstancia().modificarLista(textFieldNombreLista.getText(), textFieldNombreAgasajado.getText(), textFieldMaiAgasajado.getText(), fechaNacAgasajado, fechaFin, Float.valueOf(textFieldMontoTotal.getText()));
+					} catch (ParseException e1) {
+						e1.printStackTrace();
 					}
-					else if(resModificar > 0 & resPassRst == -1) {
-						lblMensaje.setText("No pudo modificarse la informacion");
-					}
-				
-				} catch (NumberFormatException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					JOptionPane.showMessageDialog((Component)e.getSource(), "Lista modificada");
+					
+					textFieldNombreLista.setText("");
+					textFieldNombreLista.setEnabled(true);
+					
+					textFieldNombreAgasajado.setText("");
+					textFieldNombreAgasajado.setEnabled(false);
+					
+					textFieldFechaNacAgasajado.setText("");
+					textFieldFechaNacAgasajado.setEnabled(false);
+					
+					textFieldMaiAgasajado.setText("");
+					textFieldMaiAgasajado.setEnabled(false);
+					
+					textFieldMontoTotal.setText("");
+					textFieldMontoTotal.setEnabled(false);
+					
+					textFieldFechaFin.setText("");
+					textFieldFechaFin.setEnabled(false);
+					
+					botonBuscar.setEnabled(true);
+					botonModificar.setEnabled(false);
 				}
+
+			});
+
+			JButton botonCerrar = new JButton();
+			getContentPane().add(botonCerrar);
+			botonCerrar.setText("Cerrar");
+			botonCerrar.setBounds(299, 239, 77, 28);
+			botonCerrar.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent evt) 
+				{
+					dispose();
+
 				}
-				else {
-					lblMensaje.setText("Todos los campos deben estar completos.");
-				}
-			}
-		});
-		btnConfirmarCambios.setBounds(10, 161, 171, 23);
-		contentPane.add(btnConfirmarCambios);
-		
+
+			});
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
