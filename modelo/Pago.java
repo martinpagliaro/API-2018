043664d8f731a0ced9dbff7f112b86874c6f456e
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2018-07-04 12:50:40 -0300

    Update Pago.java

--- modelo/Pago.java
+++ modelo/Pago.java
@@ -1,18 +1,53 @@
 package modelo;
 
+import java.util.Date;
+
 public class Pago {
 	
-	private int idLista;
-	private int idParticipante;
+	private Usuario usuario;
+	private Lista lista;
+	private Date fechaPago;
 	private float monto;
 	
-	public Pago(int idLista, int idParticipante, float monto) {
+	
+	public Pago(Usuario usuario, Lista lista, Date fechaPago, float monto) {
 		super();
-		this.idLista = idLista;
-		this.idParticipante = idParticipante;
+		this.usuario = usuario;
+		this.lista = lista;
+		this.fechaPago = fechaPago;
+		this.monto = monto;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Lista getLista() {
+		return lista;
+	}
+
+	public void setLista(Lista lista) {
+		this.lista = lista;
+	}
+
+	public Date getFechaPago() {
+		return fechaPago;
+	}
+
+	public void setFechaPago(Date fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+
+	public float getMonto() {
+		return monto;
+	}
+
+	public void setMonto(float monto) {
 		this.monto = monto;
 	}
 	
-	
-
 }
